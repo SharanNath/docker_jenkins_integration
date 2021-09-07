9a874d49ad51e5624437c1eddc664dca27df800a
First Last <kannanknrgmail@ip-172-31-66-130.ec2.internal>
2021-09-07 04:30:16 +0000

    first commit

---
+++ src/test/java/simplilearnproject3/docker_jenkins_integration/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package simplilearnproject3.docker_jenkins_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
